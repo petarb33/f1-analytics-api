2364670015a799b8af7d62318543d4b51535503d
petarb33 <petarbokun1@gmail.com>
2026-07-07 01:07:38 +0200

    add get_event_info method

--- app/services/plots/core/base.py
+++ app/services/plots/core/base.py
@@ -11,6 +11,19 @@ class BaseAnalysis(ABC):
     def load(self):
         self.data = fastf1.get_session(self.year, self.round_number, self.session)
         self.data.load()
+        self.event = fastf1.get_event(self.year, self.round_number)
+        self.event_info = self.get_event_info()
+
+    def get_event_info(self):
+        return {
+            "grand_prix": self.data.session_info["Meeting"]["Name"],
+            "location": self.data.session_info["Meeting"]["Circuit"]["ShortName"],
+            "country_name": self.data.session_info["Meeting"]["Country"]["Name"],
+            "country_code": self.data.session_info["Meeting"]["Country"]["Code"],
+            "round_number": self.event["RoundNumber"],
+            "session": self.data.session_info["Name"],
+            "year": self.data.session_info["StartDate"].year,
+        }
 
     def process(self):
         pass
