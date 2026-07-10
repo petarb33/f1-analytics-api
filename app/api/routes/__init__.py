40d11f5f5e03c294ffa9a416e47cec813a7a21e5
petarb33 <petarbokun1@gmail.com>
2026-07-10 14:44:45 +0200

    refactor: add session parameters to path instead of query

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -4,4 +4,8 @@ from app.api.routes import season, auth, analysis
 api_router = APIRouter()
 api_router.include_router(season.router, prefix="/season", tags=["season"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(analysis.router, prefix="/analysis", tags=["analysis"])
+api_router.include_router(
+    analysis.router,
+    prefix="/analysis/{year}/{round_number}/{session}",
+    tags=["analysis"],
+)
