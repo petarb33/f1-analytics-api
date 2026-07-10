4cc05b45404d8873af077a95fac0afd640fdc2b0
petarb33 <petarbokun1@gmail.com>
2026-07-10 17:10:14 +0200

    refactor get_sector_times to return pandas DF

--- app/services/plots/processing/sector_times.py
+++ app/services/plots/processing/sector_times.py
@@ -1,11 +1,21 @@
 from fastf1.core import Session
+import pandas as pd
 
 
-def get_sector_times(data: Session, entities: list[str], group_by: str) -> dict:
-    sector_times = {}
+def get_sector_times(data: Session, entities: list[str], group_by: str) -> pd.DataFrame:
+    rows = []
     for sector in ["Sector1Time", "Sector2Time", "Sector3Time"]:
-        sector_times[sector] = {}
         for entity in entities:
             laps = data.laps.loc[data.laps[group_by] == entity]
-            sector_times[sector][entity] = laps[sector].min().total_seconds()
-    return sector_times
+            time = laps[sector].min()
+            rows.append(
+                {
+                    "entity": entity,
+                    "sector": sector,
+                    "time": time.total_seconds(),
+                }
+            )
+
+    df = pd.DataFrame(rows)
+    df = df.sort_values("time").reset_index(drop=True)
+    return df
