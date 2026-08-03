9e926f4ca0e787db57f49b96e0c381f4864996eb
petarb33 <petarbokun1@gmail.com>
2026-08-04 00:25:45 +0200

    use settings from config.py instead of load_env

--- app/database/db.py
+++ app/database/db.py
@@ -1,13 +1,9 @@
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
+from app.core.config import settings
 
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
