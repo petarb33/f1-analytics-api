c17c1cd93e543d1b78b114e6d1d15baa98ab6f82
petarb33 <petarbokun1@gmail.com>
2026-08-05 14:48:38 +0200

    fix: DATABASE_URL -> database_url

--- app/database/db.py
+++ app/database/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
