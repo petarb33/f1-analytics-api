270e096da3ceb0ccad9d6b57e53deff690694eca
petarb33 <petarbokun1@gmail.com>
2026-08-05 14:30:34 +0200

    add QUALIFYING_SESSIONS to constants

--- app/services/constants.py
+++ app/services/constants.py
@@ -15,6 +15,8 @@ SESSIONS_MAPPING_REVERSED = {v: k for k, v in SESSIONS_MAPPING.items()}
 
 RACE_SESSIONS = ("R", "S")
 
+QUALIFYING_SESSIONS = ("Q", "SQ")
+
 CONDITION_COLORS = {"VSC": "tan", "SC": "sienna", "RF": "red"}
 
 SESSION_COLORS = {"Q1": "#b2df8a", "Q2": "#66c2a5", "Q3": "#1b7837"}
